903fdbe68096a94aec5d5af5287b7fd4292b283e
ahmetkardesseven <ahmetkardesseven@gmail.com>
2023-01-22 15:09:15 +0300

    Initial Commit

---
+++ BootCampOdevi5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BootCampOdevi5
+//
+//  Created by ahmet kardesseven on 22.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
